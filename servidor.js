f097cce42bbbaef8c2e05ea68c36b496fceda3e5
Thiago <email@example.com>
2020-08-05 15:11:48 -0300

    Criado o servidor API com express

--- servidor.js
+++ servidor.js
@@ -1,2 +1,10 @@
-servidor.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+	res.status(200).send({message: 'Olá mundo estou funcionando'})
+})
+app.listen(3001, ()=>{
+	console.log('Api rodando na porta 3011')
+})
 
