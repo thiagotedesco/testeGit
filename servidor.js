390ed94ff1c28ccc42e885b77459d880bb66f85f
thiagotedesco <30701850+thiagotedesco@users.noreply.github.com>
2020-08-05 15:18:31 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -4,6 +4,11 @@ const app = express();
 app.get('/', (req, res)=>{
 	res.status(200).send({message: 'Olá mundo estou funcionando'})
 })
+
+app.get('/ola', (req, res)=>{
+	res.status(200).send({message: 'Olá eu sou a rota ola'})
+})
+
 app.listen(3001, ()=>{
 	console.log('Api rodando na porta 3011')
 })
